81b95b85fbb81e6b5ff7c80b8d4d65fb0e8491c0
司馬鴻 <sobukia@shibahiroshinoMacBook-ea.local>
2021-09-04 16:59:16 +0900

    初めてのコミットです。

---
+++ TestForGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestForGit
+//
+//  Created by 司馬鴻 on 2021/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("1")
+    }
+
+
+}
+
